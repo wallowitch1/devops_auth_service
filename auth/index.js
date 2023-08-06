52a40f7e757535967ef51413c29c262c2b7d1b8d
wallowitch2 <wallowitch2@gmail.com>
2023-08-06 15:37:03 +0900

    add loging

--- auth/index.js
+++ auth/index.js
@@ -17,6 +17,12 @@ app.set('views', path.join(__dirname, 'views'));
 
 const SECRET_KEY = "keroro2424.";
 
+// 로그 파일 스트림 생성
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+// morgan을 사용하여 액세스 로그 설정
+app.use(morgan('combined', { stream: accessLogStream }));
+
 app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
